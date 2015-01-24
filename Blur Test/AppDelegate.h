d4586925e77366dd87446571c35f21201c95bb83
Boris <zhouboli@gmail.com>
2015-01-25 01:03:59 +0800

    Initial Commit

---
+++ Blur Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blur Test
+//
+//  Created by Boris Chou on 1/25/15.
+//  Copyright (c) 2015 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blur Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blur Test
+//
+//  Created by Boris Chou on 1/25/15.
+//  Copyright (c) 2015 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
